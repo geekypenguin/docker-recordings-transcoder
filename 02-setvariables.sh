a5357e7bf0db0b3eb332cb219370d01dd7ab6ea8
djaydev <33335484+djaydev@users.noreply.github.com>
2019-07-25 20:44:47 -0400

    update scripts

--- 02-setvariables.sh
+++ 02-setvariables.sh
@@ -1,20 +1,22 @@
 #!/usr/bin/with-contenv bash
 if [ ${ENCODER} = "nvidia" ] ; then
-echo ENCODER_SCRIPT=nvidia >> /scripts/setvariables.txt
+ENCODER_SCRIPT=nvidia
 fi
 if [ ${ENCODER} = "intel" ] ; then
-echo ENCODER_SCRIPT=intel >> /scripts/setvariables.txt
+ENCODER_SCRIPT=intel
 fi
 if [ ${ENCODER} = "software" ] ; then
-echo ENCODER_SCRIPT=software >> /scripts/setvariables.txt
+ENCODER_SCRIPT=software
 fi
 if [ ${SUBTITLES} = "0" ] ; then
-echo ENCODER_SCRIPT_END=.sh >> /scripts/setvariables.txt
+ENCODER_SCRIPT_END=.sh
 fi
 if [ ${SUBTITLES} = "1" ] ; then
-echo ENCODER_SCRIPT_END=-subtitles.sh >> /scripts/setvariables.txt
+ENCODER_SCRIPT_END=-subtitles.sh
 fi
 if [ ${DELETE_TS} = "1" ] ; then
 echo "rm \"\$1\"" | tee /scripts/*.sh > /dev/null
 fi
 chmod +x /scripts/*
+sed -i "s/ENCODER/$ENCODER_SCRIPT/g" /etc/services.d/autoconvert/run
+sed -i "s/END/$ENCODER_SCRIPT_END/g" /etc/services.d/autoconvert/run
--- startapp.sh
+++ startapp.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/with-contenv bash
-source /scripts/setvariables.txt
 exec watchmedo shell-command \
     --patterns="*.ts" \
     --recursive \
-    --command='/bin/bash /scripts/${ENCODER_SCRIPT}${ENCODER_SCRIPT_END} ${watch_src_path}' \
+    --command='/scripts/ENCODEREND ${watch_src_path}' \
     /watch
